b8d793715f72084d2be015227d90fa152de4f1d2
Corbin Benally <benallyc@unlv.nevada.edu>
2012-04-22 22:34:59 -0700

    rough drafts and several revisions; rough documentation added

---
+++ roughStrategy.java
@@ -0,0 +1,36 @@
+ArrayList arrList = new arrList(); //list of users which are passed-in
+String[] userArray; //local array which stores names of users
+userArray = new String[arrList.size()];
+Double[] ratioArray; //another local array which stores ratios of users
+ratioArray = new String[arrList.size()];
+	
+Iterator itr = al.iterator(); 
+for (int i = 0; i < arrList.size(); i++) {
+
+    userArray[i] = arrList[i];
+	switch (category){
+	
+		case 'driveRatio':
+			driveRatioOnly(arrayList[i]);
+	
+		case 'driveRatioWithLies':
+			
+}
+
+
+
+
+
+
+
+
+public void driveRatioOnly(User) {
+		//double[] totalArray;
+		//double lowestRatio;
+		String userToDrive;
+		totalArray = new double[numUsersParticipating];
+		double dgRatio = User.getTimesDrove() / User.getTimesGone();
+	
+		
+	
+	}
